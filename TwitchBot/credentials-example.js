1de2bbfb7b502383db9d28f8bf88a263a148c80b
Lawrence Lai <laitcl@umich.edu>
2019-06-27 14:07:19 -0400

    Removed obsolete scripts and ipython notebooks; added example credential files

---
+++ TwitchBot/credentials-example.js
@@ -0,0 +1,12 @@
+const opts = {
+  identity: {
+    username: "Twitch Bot Username",
+    password: "oauth:secretkey"
+  },
+  channels: [
+    "channel1",
+    "channel2",
+    "channel3",
+  ]
+};
+exports.opts = opts ;
