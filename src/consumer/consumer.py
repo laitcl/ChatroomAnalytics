219ccac31704a5e268c2109a7a1788acecdbf17c
laitcl <laitcl@umich.edu>
2019-07-02 14:12:49 -0400

    Removed sensitive information

---
+++ src/consumer/consumer.py
@@ -0,0 +1,143 @@
+# Import Dependencies
+import datetime
+import io
+import threading
+import re
+import operator
+import os
+import time
+from time import gmtime, strftime
+from keras.models import model_from_json
+import pickle
+from kafka import KafkaConsumer
+import psycopg2
+import tensorflow as tf
+# Import my functions from the sentiment analysis
+import Intent_Classification_Lai
+from Intent_Classification_Lai import predictions
+from Intent_Classification_Lai import get_final_output
+
+
+# Establish Connection to PostGres Postgres Information
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(os.path.join(__location__,'PostgresPass.txt'), 'r') as file:
+    hostname = file.readline().split("\n")[0]
+    passwordstring = file.readline().split("\n")[0]
+try:
+    conn = psycopg2.connect(user="laitcl",
+                                 password=passwordstring,
+                                 host=hostname,
+                                 port="5432",
+                                 database="testpython")
+    # create a psycopg2 cursor that can execute queries
+    cur = conn.cursor()
+    # create a new table with a single column called "name"
+    print('Connection to PostgreSQL successful')
+except Exception as e:
+    print("Can't connect. Invalid dbname, user or password?")
+    print(e)
+
+# Define Functions
+#Method to process one line of messages
+def processline(line):
+    [date, channel, text] = line.split(",", 2)
+    [date, timeofday] = date.split("_", 1)
+    text = text.split("\r", 1)[0]
+    return [date, timeofday, channel, text]
+
+# Load chat classification model
+# Model reconstruction from JSON file
+with open(os.path.join(__location__,'../../tools/IntentClassification/model_architecture.json'), 'r') as f1:
+    model = model_from_json(f1.read())
+
+# Load weights into the new model
+model.load_weights(os.path.join(__location__,'../../tools/IntentClassification/model_weights.h5'))
+
+# Load Word Tokenizer
+with open(os.path.join(__location__,'../../tools/IntentClassification/tokenizer.pickle'), 'rb') as handle:
+    word_tokenizer = pickle.load(handle)
+
+# Load Max Length of a message
+with open(os.path.join(__location__,'../../tools/IntentClassification/maxlen.txt'), 'r') as f2:
+    max_length = int(f2.readline())
+
+# Setup Kafka Consumer
+
+with open(os.path.join(__location__,'Kafkahosts.txt'), 'r') as file2:
+    server1 = file2.readline().split("\n")[0]
+    server2 = file2.readline().split("\n")[0]
+topics = ['twitchmessages']
+consumer = KafkaConsumer(
+    *topics,
+     bootstrap_servers=[server1, server2],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     group_id='my-group')
+
+#Setup channel analytics class for consumer code
+class channelanalytics:
+    def __init__(self):
+        #Logging Channel Analytics
+        self.channelnumlines = {}
+        self.channelsentiments = {}
+        self.unique_intent = ['question', 'disappointment', 'funny', 'neutral']
+        #Logging Time
+        self.beginningtime = time.time()
+        self.starttime = time.time()
+        self.logtimeinterval = 5
+
+    def initializecounter(self, channel):
+        self.channelnumlines[channel] = 0
+        self.channelsentiments[channel] = {}
+        for intent in self.unique_intent:
+            self.channelsentiments[channel][intent] = 0
+
+    def incrementchannel(self, channel, message):
+        self.channelnumlines[channel] += 1
+        sentiment = get_final_output(
+        predictions(
+            word_tokenizer,
+            text,
+            model,
+            max_length),
+            self.unique_intent,
+             'classify')#Perform an intent classification
+        self.channelsentiments[channel][sentiment] += 1
+
+    def databaseupdate(self,channel):
+        dateandtime = str(time.asctime())
+        numlines = self.channelnumlines[channel]
+        lastsentiment = {}
+        for intent in self.unique_intent:
+            lastsentiment[intent] = self.channelsentiments[channel][intent]
+        sql = """INSERT INTO ChatAnalytics (channelname, date, time, nummessages, question, disappointment, funny, neutral)
+        VALUES(%s, CURRENT_DATE, CURRENT_TIME, %s, %s, %s, %s, %s);"""
+        cur.execute(
+        sql,
+        (channel,
+        numlines,
+        lastsentiment['question'],
+        lastsentiment['disappointment'],
+        lastsentiment['funny'],
+        lastsentiment['neutral']))
+        conn.commit()
+        print("Data point committed at ", datetime.datetime.now())
+
+if __name__ == '__main__':
+    # Setup tracking variables
+    channels = channelanalytics()
+    # begin logging chats
+    try:
+        for message in consumer:
+            line = message.value  # Takes message from consumer object
+            [date, timeofday, channel, text]= processline(line.decode())  # Process contents of the message
+            if channel not in channels.channelnumlines:  # If channel wasn't previously tracked, start tracking
+                channels.initializecounter(channel)
+            channels.incrementchannel(channel, text)# Increment a sentiment and number of lines
+            if time.time() - channels.starttime >= channels.logtimeinterval:# Every interval, perform analysis
+                for channel in channels.channelnumlines:
+                    channels.databaseupdate(channel)#Export information to database
+                    channels.initializecounter(channel)#Reinitalize counter for next cycle
+                channels.starttime = time.time()  # Reset the start time
+    except KeyboardInterrupt:  # Let user stop logging when keyboard command is sent
+        conn.close()  # Close connection after everything is done
---
+++ tools/ChromeExtension/Flask/App.py
@@ -0,0 +1,112 @@
+# ./app.py
+from flask import Flask, request, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from pusher import Pusher
+import requests, json, atexit, time, plotly, plotly.graph_objs as go
+import psycopg2
+import operator
+import time
+
+# create flask app
+app = Flask(__name__)
+
+# configure pusher object
+with open ('pusherinfo.txt', 'r') as file:
+    secretstring = file.readline().split("\n")[0]
+    pusher = Pusher(
+        app_id='811964',
+        key='114fb576b66e1ffc4165',
+        secret=secretstring,
+        cluster='mt1',
+        ssl=True
+    )
+
+#Establish Connection to PostGres Postgres Information
+with open ('PostgresPass.txt', 'r') as file2:
+    hostname = file2.readline().split("\n")[0]
+    passwordstring = file2.readline().split("\n")[0]
+try:
+    conn = psycopg2.connect(user = "laitcl",
+                                 password = passwordstring,
+                                 host = hostname,
+                                 port = "5432",
+                                 database = "testpython")
+    # create a psycopg2 cursor that can execute queries
+    cur = conn.cursor()
+    # create a new table with a single column called "name"
+    print('Connection to PostgreSQL successful')
+except Exception as e:
+    print("Can't connect. Invalid dbname, user or password?")
+    print(e)
+
+
+# define variables for data retrieval
+times = []
+nummessages = []
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+def retrieve_data():
+    #Query from PostgreSQL
+    timetocollect = 60
+    channelname = 'gamesdonequick'
+    SQL = "select * from chatanalytics where (extract(epoch from now()::timestamp)-extract(epoch from (date ||' '||time)::timestamp))<%d and channelname = '%s';" % (timetocollect, channelname)
+    cur.execute(SQL)
+    sqldata = cur.fetchall()
+
+    #Process query
+    timevector =[]
+    nummessagevector = []
+    sentimentvector = []
+    sentiment = {}
+
+    for datapoint in sqldata:
+        timevector.append(datapoint[3].strftime("%H:%M:%S"))
+        nummessagevector.append(datapoint[4])
+        nquestion = datapoint[5]
+        ndisappointment = datapoint[6]
+        nfunny = datapoint[7]
+        nneutral = datapoint[8]
+        sentiment["question"] = nquestion
+        sentiment["disappointment"] = ndisappointment
+        sentiment["funny"] = nfunny
+        sentiment["neutral"] = nneutral
+        latestchannelsentiment = max(sentiment.items(), key=operator.itemgetter(1))[0]
+        sentimentvector.append(latestchannelsentiment)
+
+
+    # create an array of traces for graph data
+    graph_data = [go.Scatter(
+        x=timevector,
+        y=nummessagevector,
+        mode='lines+markers+text',
+        text=sentimentvector,
+        textposition='bottom center',
+        name="%s analytics" % (channelname))]
+
+    data = {
+        'graph': json.dumps(list(graph_data), cls=plotly.utils.PlotlyJSONEncoder),
+    }
+
+    # trigger event
+    pusher.trigger("crypto", "data-updated", data)
+
+# create schedule for retrieving prices
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=retrieve_data,
+    trigger=IntervalTrigger(seconds=5),
+    id='chat_analytics_retrieval_job',
+    name='Retrieve prices every 5 seconds',
+    replace_existing=True)
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+
+# run Flask app
+app.run(debug=True, use_reloader=False)
